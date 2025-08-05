bdc91e1f8d475c3ef50df50609e90fd783d969c6
Nico Wagner <info@ni-wa.de>
2025-08-05 23:11:23 +0200

    build: update to 12.1.252.0
    * Improve docker build steps to reduce image size

--- files/healthcheck.sh
+++ files/healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl http://127.0.0.1:8080/era/webconsole/ | grep '<title>ESET PROTECT on-prem</title>' || exit 1
+curl http://127.0.0.1:8080/era/webconsole/ | grep '<title>ESET PROTECT On-Prem</title>' || exit 1
